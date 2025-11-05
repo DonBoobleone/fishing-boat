e4a271316c1b78550d66a1d9391600e24f3dba82
Don Boobleone <donboobleone@gmail.com>
2025-11-06 00:04:23 +0100

    AAI vehichles compatibilitity quickfix

--- control.lua
+++ control.lua
@@ -1,6 +1,11 @@
 -- control.lua
 require("scripts.enter-exit")
-require("scripts.fishing")
+
+if script.active_mods["aai-programmable-vehicles"] then
+    require("scripts.fishing-aai-compa")
+else
+    require("scripts.fishing")
+end
 
 if settings.startup["thanks-for-all-the-fish"].value then
     require("scripts.breeding-fish")
